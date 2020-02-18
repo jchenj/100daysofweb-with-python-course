e3e2be7c0f3f908e56fe490886375a888eb4b2e1
Julianna Chen <c.julianna@gmail.com>
2020-02-18 22:11:18 +0800

    update with work on HTML/HTML5

--- days/001-004-flask-intro/code/program/routes.py
+++ days/001-004-flask-intro/code/program/routes.py
@@ -7,8 +7,15 @@ from program import app
 def index():
     return render_template('index.html')
     # Extra params for return render_template: title = 'Template Demo', time = timenow
+    # Extra params didn't work when I tried them
+    # TODO: understand what extra params do & how to make them work
 
 
 @app.route('/100Days')
 def p100days():
     return render_template('100Days.html')
+
+
+
+
+
