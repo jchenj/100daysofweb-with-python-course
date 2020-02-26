bd35d6f860accb526ef5ad36a96f2c327ed0ef00
Julianna Chen <c.julianna@gmail.com>
2020-02-26 15:21:15 +0800

    changes to app & test data

---
+++ days/009-012-modern-apis-starred/my_demo/app.py
@@ -0,0 +1,72 @@
+import json
+from typing import List
+
+from apistar import App, Route, types, validators
+from apistar.http import JSONResponse
+
+
+# helpers
+
+def _load_cars_data():
+    with open('mock_cars.json') as f:
+        cars = json.loads(f.read())
+        return {car["id"]: car for car in cars}
+
+
+# load in data
+
+cars = _load_cars_data()
+
+# define some vars and consts
+
+VALID_MANUFACTURERS = set([car["manufacturer"]
+                           for car in cars.values()])
+CAR_NOT_FOUND = "Car not found"
+# breakpoint()    used this for testing after wrote code above this point
+
+
+# definition
+
+class Car(types.type):
+    id = validators.Integer(allow_null=True)    # assign in POST
+    manufacturer = validators.String(enum=list(VALID_MANUFACTURERS))
+    model = validators.String(max_length=50)
+    year = validators.Integer(minimum=1900, maximum=2050)
+    vin = validators.String(max_length=50, default='')
+
+
+# API methods
+
+def list_cars() -> List[Car]:
+    pass
+
+
+def create_car(car: Car) -> JSONResponse:
+    pass
+
+
+def get_car(car_id: int) -> JSONResponse:
+    pass
+
+
+def update_car(car_id: int) -> JSONResponse:
+    pass
+
+
+def delete_car(car_id: id) -> JSONResponse:
+    pass
+
+
+routes = [
+    Route('/', method='GET', handler=list_cars)
+    Route('/', method='POST', handler=create_car)
+    Route('/{car_id}/', method='GET', handler=get_car)
+    Route('/{car_id}/', method='PUT', handler=update_car)
+    Route('/{car_id}/', method='DELETE', handler=delete_car)                                                                                                                               =delete_car)
+]
+
+app = App(routes=routes)
+
+
+if __name__ == '__main__':
+    app.serve('127.0.0.1', 5000, debug=True)
